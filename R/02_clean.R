a6a5d76d942d733385b919ce6b07021d94c8c10d
abrildh <abril.diosdadoh@gmail.com>
2020-04-17 09:11:39 +0000

    Initial cleaning

---
+++ R/02_clean.R
@@ -0,0 +1,56 @@
+# Clear workspace
+# ------------------------------------------------------------------------------
+rm(list = ls())
+
+# Load libraries
+# ------------------------------------------------------------------------------
+library(tidyverse)
+
+# Define functions
+# ------------------------------------------------------------------------------
+source(file = "R/99_project_functions.R")
+
+# case_df cleaning
+# ------------------------------------------------------------------------------
+case_df <- case_df %>% 
+  mutate(city = replace (city, city == "-", "other")) %>% 
+  mutate(city = replace (city, city == "from other city", "other")) %>% 
+  mutate(infection_case = replace(infection_case, infection_case == "etc", "other")) %>% 
+  mutate(latitude = replace(latitude, latitude == "-", NA)) %>% 
+  mutate(longitude = replace(longitude, longitude == "-", NA))
+
+# patient_df cleaning
+# ------------------------------------------------------------------------------
+#Delete unwanted columns
+patient_df$disease <- NULL
+patient_df$contact_number <- NULL
+patient_df$global_num_patient_route <- NULL
+#we can use select instead of NULL
+
+patient_df <- patient_df %>% 
+  mutate(infection_case = replace_na(infection_case, "other")) %>% 
+  mutate(infection_case = replace(infection_case, infection_case == "etc", "other")) %>% 
+  mutate(state = replace_na(state, "unspecified")) %>% 
+  mutate(sex = case_when(sex == "male" ~ 0,
+                         sex == "female" ~ 1)) %>% 
+#mutate(sex = replace_na(sex, "unspecified")) 
+  mutate(country = replace_na(country, "other")) %>% 
+  mutate(type = replace_na(type, "other")) %>%
+  mutate(type = replace(type, type == "etc", "other")) %>% 
+  mutate(province_patient_route = replace(province_patient_route, province_patient_route == "etc", "unspecified")) %>% 
+  mutate(year_new = 2020 - birth_year) 
+  
+
+#### mutate(year_new = 2020 - birth_year)  %>% 
+# mutate(year_new = case_when( year_new < 10 ~ "0s",
+#                            year_new <= 10 ~ "10s", 
+#                             year_new <= 20 ~ "20s",
+#year_new <= 30 ~ "30s",
+#year_new <= 40 ~ "40s",
+#year_new <= 50 ~ "50s",
+#year_new <= 60 ~ "50s",
+#year_new <= 70 ~ "70s",
+#year_new <= 80 ~ "80s",
+#year_new <= 90 ~ "90s",
+#year_new >= 100 ~ "100s"))
+
