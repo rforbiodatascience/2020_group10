9ed5fff8330aa86c9eab3ea41a0d1ef4b1876c5a
s190171 <s190171@student.dtu.dk>
2020-04-21 09:10:47 +0000

    Cleaning of the region df (split into 3 different dfs for tidyness). Move time_df augmenting to correct script.

--- R/03_augment.R
+++ R/03_augment.R
@@ -22,27 +22,28 @@ patient_df <- read_tsv("data/patient_data_clean.tsv", guess_max = 3000)
 # ------------------------------------------------------------------------------
 time_df <- read_tsv("data/time_data_clean.tsv")
 
+# Adding a binary gender column
+time_df <- time_df %>% 
+  mutate(sex = case_when(sex == "male" ~ 0,
+                         sex == "female" ~ 1))
+
+#Removing unwanted columns (time*4)
+time_df <- time_df %>% select(-time, -time_time_age, -time_time_gender, -time_time_province)
+
+# Write to disk
+write_tsv(time_df, "data/time_data_augmented.tsv")
+
 # Region augment
 # ------------------------------------------------------------------------------
-region_df <- read_tsv("data/region_data_clean.tsv")
+city_df <- read_tsv("data/city_data_clean.tsv")
 
-region_df <-region_df %>%
+# Use the suffixes to identify divisions in
+city_df <-city_df %>%
   mutate(division = case_when(str_ends(city, "-do") ~ "province",
                               str_ends(city, "-si") ~ "city",
                               str_ends(city, "-gun") ~ "county",
                               str_ends(city, "-gu") ~ "district",
                               TRUE ~ "city"))
-
-province_summary <- region_df %>%
-  filter(province == city) %>%
-  group_by(province) %>%
-  summarise_at(vars(contains("count")), sum, na.rm = FALSE)
-
-city_summary <- region_df %>% 
-  filter(province != city) %>%
-  group_by(province) %>%
-  summarise_at(vars(contains("count")), sum, na.rm = FALSE)
-
-full_join(province_summary, city_summary, by = "province")
-
+# Write to disk
+write_tsv(city_df, "data/city_data_augmented.tsv")
 
