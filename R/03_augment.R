a130c2804f525d92cecc94cfa1d288dc4f402317
abrildh <abril.diosdadoh@gmail.com>
2020-04-22 14:16:29 +0000

    Augment patient_df

--- R/03_augment.R
+++ R/03_augment.R
@@ -18,6 +18,29 @@ case_df <- read_tsv("data/case_data_clean.tsv")
 # ------------------------------------------------------------------------------
 patient_df <- read_tsv("data/patient_data_clean.tsv", guess_max = 3000)
 
+patient_df <- patient_df %>% 
+  #transform gender to binary
+  mutate(sex = case_when(sex == "male" ~ 0,
+                       sex == "female" ~ 1)) %>% 
+  #add an age column and one more column to subset the ages into age_group
+  mutate(age = 2020 - birth_year) %>%
+  mutate(age_group = case_when( birth_year < 10 ~ "0s",
+                                age < 20 ~ "10s", 
+                                age < 30 ~ "20s",
+                                age < 40 ~ "30s",
+                                age < 50 ~ "40s", 
+                                age < 60 ~ "50s",
+                                age < 70 ~ "60s",
+                                age < 80 ~ "70s",
+                                age < 90 ~ "80s",
+                                age < 100 ~ "90s",
+                                age >= 100 ~ "100s")) %>%
+  select(-birth_year)
+
+# Write to disk
+write_tsv(patient_df, "data/patient_data_augmented.tsv")
+
+
 # Time augment
 # ------------------------------------------------------------------------------
 time_df <- read_tsv("data/time_data_clean.tsv")
