d5a3a64c9a7ea425d00c4195b9169bb5f59ab8cb
eryk <erik.kiehl@hotmail.dk>
2020-05-03 13:02:11 +0200

    PCA plot and correlation heatmap

---
+++ R/99_proj_func.R
@@ -0,0 +1,8 @@
+
+# Get lower tri ------------------------------------------------------------
+
+# Gets lower triangle of correlation matrix
+get_lower_tri <- function(cormat) {
+  cormat[upper.tri(cormat)] <- NA
+  return(cormat)
+}
