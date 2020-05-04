9c6b334eea5e3a1ec5ec2fbcb65ad286fc445613
abrildh <abril.diosdadoh@gmail.com>
2020-05-04 06:15:10 +0000

    slight code changes

--- R/model_patient_route.R
+++ R/model_patient_route.R
@@ -28,12 +28,8 @@ patient_df <- read_tsv("data/patient_data_augmented.tsv")
 lon_lan <- patient_df %>% 
   dplyr::select(.,c(longitude, latitude, sex, age_group, province_patient_route, state)) %>% 
   filter(!is.na(c(longitude))) %>% 
-  mutate(sex = as_factor(sex)) #%>% 
-#  mutate(state = case_when(state == "released" ~ 0,
-#                           state == "deceased" ~ 1, 
-#                           state == "isolated" ~ 2
-#                           )) %>% 
-#  mutate(state = as_factor(state)) 
+  mutate(sex = as_factor(sex))  
+
 
 range_coordinates <- lon_lan %>% 
   dplyr::select(.,c(province_patient_route, state, longitude, latitude)) %>%
@@ -49,7 +45,7 @@ cases_number <- lon_lan %>%
   tally() %>% 
   as_tibble
 
-new_table <- range_coordinates %>% 
+cases_number <- range_coordinates %>% 
   full_join(cases_number,
             by= c("state","province_patient_route"))
 
@@ -78,23 +74,25 @@ sk_age_group <- ggplot() +
 
 #Data visualization by number
 # ------------------------------------------------------------------------------
-otro <- ggplot() +
+sk_cases_number <- ggplot() +
   geom_polygon(data= south_korea, aes(x=long, y = lat, group = group), fill="grey", alpha=0.4) +
   theme_void() +
   coord_map() +
-  geom_point( data = new_table, aes( y=min_latt,  x=max_lonn, color= state, size= n)) +
+  geom_point( data = cases_number, aes( y=min_latt,  x=max_lonn, color= state, size= n)) +
   scale_color_viridis(option= "inferno", discrete=TRUE, alpha= 0.7)  +
   scale_alpha_continuous() + 
   scale_size_continuous(range= c(1,15), name= "Number of patients") +
   guides(colour = guide_legend())
-otro
+
 
 # Save the plots 
 # ------------------------------------------------------------------------------
-ggsave(filename = "results/05_plot_state.png", plot = sk_state)
-ggsave(filename = "results/05_plot_age.png", plot = sk_age_group)
+ggsave(filename = "results/plot_state.png", plot = sk_state)
+ggsave(filename = "results/plot_age.png", plot = sk_age_group)
+ggsave(filename = "results/plot_cases_number.png", plot = sk_cases_number)
 
 # Save the data frame 
 # ------------------------------------------------------------------------------
 write_tsv(x = lon_lan, path = "data/wrangled_longitude_latitude_df.tsv")
+write_tsv(x = cases_number, path = "data/wrangled_cases_number_df.tsv")
  
