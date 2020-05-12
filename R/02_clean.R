822f7e891ff03d357d1d1d00598c1e940d45f904
eryk <erik.kiehl@hotmail.dk>
2020-05-12 16:59:10 +0200

    Cleaned cleaning

--- R/02_clean.R
+++ R/02_clean.R
@@ -1,26 +1,41 @@
 # Clear workspace ------------------------------------------------------------------------------
 rm(list = ls())
 
-# Load libraries ------------------------------------------------------------------------------
+# Load libraries -------------------------------------------------------------------------------
 library(tidyverse)
 
-# Define functions ------------------------------------------------------------------------------
+# Define functions -----------------------------------------------------------------------------
 source(file = "R/99_project_functions.R")
 
-# Wrangle data ------------------------------------------------------------------------------
-
-# case_df cleaning
+# Read the data --------------------------------------------------------------------------------
 case_df <- read_tsv("data/case_data.tsv")
-case_df <- case_df %>%
-  mutate(city = replace(city, city == "-", "other")) %>%
-  mutate(city = replace(city, city == "from other city", "other")) %>%
-  mutate(infection_case = replace(infection_case, infection_case == "etc", "other")) %>%
-  mutate(latitude = replace(latitude, latitude == "-", NA)) %>%
-  mutate(longitude = replace(longitude, longitude == "-", NA))
 
-# patient_info_df cleaning
-# Increase guess_max to read all columns the right type
+# guess_max for correct type columns
 patient_info_df <- read_tsv("data/patient_info_data.tsv", guess_max = 3000)
+patient_route_df <- read_tsv("data/patient_route_data.tsv")
+
+time_df <- read_tsv("data/time_data.tsv")
+time_age_df <- read_tsv("data/time_age_data.tsv")
+time_gender_df <- read_tsv("data/time_gender_data.tsv")
+time_province_df <- read_tsv("data/time_province_data.tsv")
+
+search_trend_df <- read_tsv("data/search_trend_data.tsv")
+
+region_df <- read_tsv("data/region_data.tsv")
+
+# Cleaning case dataframe  ---------------------------------------------------------------------
+
+# Replace unknowns to same identity
+case_df <- case_df %>%
+  mutate(
+    city = replace(city, city == "-", "other"),
+    city = replace(city, city == "from other city", "other"),
+    infection_case = replace(infection_case, infection_case == "etc", "other"),
+    latitude = replace(latitude, latitude == "-", NA),
+    longitude = replace(longitude, longitude == "-", NA)
+  )
+
+# Cleaning patient info -------------------------------------------------------------------------
 
 # Delete unwanted columns patient_info_df
 patient_info_df <- patient_info_df %>%
@@ -28,90 +43,81 @@ patient_info_df <- patient_info_df %>%
 
 # Clean up inconsistent string values
 patient_info_df <- patient_info_df %>%
-  mutate(infection_case = replace_na(infection_case, "other")) %>%
-  mutate(infection_case = replace(infection_case, infection_case == "etc", "other")) %>%
-  mutate(state = replace_na(state, "unspecified")) %>%
-  mutate(country = replace_na(country, "other")) %>%
-  mutate(province = replace(province, province == "etc", "other")) %>%
-  mutate(city = replace(city, city == "etc", "unspecified")) %>%
-  mutate(city = replace_na(city, "unspecified"))
+  mutate(
+    infection_case = replace_na(infection_case, "other"),
+    infection_case = replace(infection_case, infection_case == "etc", "other"),
+    state = replace_na(state, "unspecified"),
+    country = replace_na(country, "other"),
+    province = replace(province, province == "etc", "other"),
+    city = replace(city, city == "etc", "unspecified"),
+    city = replace_na(city, "unspecified")
+  )
 
-# Convert infected_by to NA if two persons both have infected each other
-# or if they have infected themselves
+# Remove self infection and make new variable of infected and infecting person.
 patient_info_df <- patient_info_df %>%
   mutate(infected_by = replace(infected_by, infected_by == patient_id, NA)) %>%
   rowwise() %>%
   mutate(inf_pair = str_c(sort(c(patient_id, infected_by)), collapse = ",")) %>%
-  ungroup() %>%
+  ungroup()
+
+# Introduce NA where two people are eachothers infected_by
+patient_info_df <- patient_info_df %>%
   group_by(inf_pair) %>%
-  mutate(
-    infected_by = case_when(
-      n() == 1 ~ infected_by
-    )
-  ) %>%
+  mutate(infected_by = case_when(
+    n() == 1 ~ infected_by 
+  )) %>%
   ungroup() %>%
   select(-inf_pair)
 
-# patient_route_df cleaning
-patient_route_df <- read_tsv("data/patient_route_data.tsv")
+# Cleaning patient route dataframe --------------------------------------------------------------
 
 # Delete unwanted columns patient_route_df
 patient_route_df <- patient_route_df %>%
   select(-global_num)
 
+# Clean up inconsistent string values
 patient_route_df <- patient_route_df %>%
-  mutate(type = replace_na(type, "other")) %>%
-  mutate(type = replace(type, type == "etc", "other")) %>%
-  mutate(province = replace(province, province == "etc", "unspecified")) %>%
-  mutate(province = replace_na(province, "unspecified")) %>%
-  mutate(city = replace_na(city, "unspecified"))
+  mutate(
+    type = replace_na(type, "other"),
+    type = replace(type, type == "etc", "other"),
+    province = replace(province, province == "etc", "unspecified"),
+    province = replace_na(province, "unspecified"),
+    city = replace_na(city, "unspecified")
+  )
 
-# Region data cleaning
-region_df <- read_tsv("data/region_data.tsv")
+# Cleaning region dataframe ---------------------------------------------------------------------
 
-# Filter cities and small regions
-# (add prefix to city for small regions with no explicitly stated cities)
-
+# Summarize the amount of reoccuring provinces
 city_df <- region_df %>%
-  filter(province != city | city == "Jeju-do" | city == "Sejong") %>%
+  group_by(province) %>%
+  mutate(count = length(province)) %>%
+  ungroup()
+
+# Remove rows with same cities as province, unless the province only exist once
+city_df <- city_df %>%
+  filter(province != city & province != "Korea" | count < 2) %>%
+  select(-count)
+
+# Add prefix to cities that are named same as province
+city_df <- city_df %>% 
   mutate(city = case_when(
-    city == province ~ str_c("unknown_", city),
+    city == province ~ str_c(city, "_city"),
     TRUE ~ city
   ))
 
-# Filter for provinces
-province_df <- region_df %>%
-  filter(province == city & province != "Korea")
+# Write data to file ----------------------------------------------------------------------------
 
-# Write data to file --------------------------------------------
-
-# Write clean case_df
 write_tsv(case_df, "data/case_data_clean.tsv")
 
-# Write clean time_df
-time_df <- read_tsv("data/time_data.tsv")
 write_tsv(time_df, "data/time_data_clean.tsv")
-
-# Write clean time_age_df
-time_age_df <- read_tsv("data/time_age_data.tsv")
 write_tsv(time_age_df, "data/time_age_data_clean.tsv")
-
-# Write clean time_gender_df
-time_gender_df <- read_tsv("data/time_gender_data.tsv")
 write_tsv(time_gender_df, "data/time_gender_data_clean.tsv")
-
-# Write clean case_df
-time_province_df <- read_tsv("data/time_province_data.tsv")
 write_tsv(time_province_df, "data/time_province_data_clean.tsv")
 
-# Write clean patient data
 write_tsv(patient_route_df, "data/patient_route_data_clean.tsv")
 write_tsv(patient_info_df, "data/patient_info_data_clean.tsv")
 
-# Write clean search_trend_df
-search_trend_df <- read_tsv("data/search_trend_data.tsv")
 write_tsv(search_trend_df, "data/search_trend_data_clean.tsv")
 
-# Write clean region data to disk
 write_tsv(province_df, "data/province_data_clean.tsv")
 write_tsv(city_df, "data/city_data_clean.tsv")
--- R/03_augment.R
+++ R/03_augment.R
@@ -1,16 +1,27 @@
-# Clear workspace ------------------------------------------------------------------------------
+# Clear workspace -------------------------------------------------------------------------------
 rm(list = ls())
 
-# Load libraries ------------------------------------------------------------------------------
+# Load libraries --------------------------------------------------------------------------------
 library(tidyverse)
 
 # Define functions ------------------------------------------------------------------------------
 source(file = "R/99_project_functions.R")
 
-# Wrangle data ------------------------------------------------------------------------------
-
-# Case augment
+# Read the data ---------------------------------------------------------------------------------
 case_df <- read_tsv("data/case_data_clean.tsv")
+
+patient_info_df <- read_tsv("data/patient_info_data_clean.tsv")
+patient_route_df <- read_tsv("data/patient_route_data_clean.tsv")
+
+time_df <- read_tsv("data/time_data_clean.tsv")
+time_age_df <- read_tsv("data/time_age_data_clean.tsv")
+time_gender_df <- read_tsv("data/time_gender_data_clean.tsv")
+time_province_df <- read_tsv("data/time_province_data_clean.tsv")
+search_trend_df <- read_tsv("data/search_trend_data_clean.tsv")
+
+city_df <- read_tsv("data/city_data_clean.tsv")
+
+# Augment case dataframe ------------------------------------------------------------------------
 
 case_df <- case_df %>%
   mutate(case_type = case_when(
@@ -32,20 +43,18 @@ case_df <- case_df %>%
 case_df <- case_df %>%
   select(confirmed, case_type)
 
-# Patient augment
-patient_info_df <- read_tsv("data/patient_info_data_clean.tsv")
-patient_route_df <- read_tsv("data/patient_route_data_clean.tsv")
+# Augment patient dataframes --------------------------------------------------------------------
 
 # Joining the the two patient data frames
 patient_df <- patient_info_df %>%
-  full_join(patient_route_df,
-    by = "patient_id",
-    suffix = c("_patient_info", "_patient_route"))
+  full_join(patient_route_df, by = "patient_id", suffix = c("_patient_info", "_patient_route"))
 
+# Remove unnecessary columns
 patient_df <- patient_df %>%
-  # Remove unnecessary columns
   select(-sex) %>%
-  # add an age column and one more column to subset the ages into age_group
+  
+# Add age column and one more column to subset the ages into age_group
+patient_df <- patient_df %>%
   mutate(age = 2020 - birth_year) %>%
   mutate(age_group = case_when(
     age < 10 ~ "0s",
@@ -72,27 +81,13 @@ patient_df <- patient_df %>%
 patient_df <- patient_df %>%
   select(-c(global_num, country, symptom_onset_date, age))
 
-# Time augment
-time_df           <- read_tsv("data/time_data_clean.tsv")
-time_age_df       <- read_tsv("data/time_age_data_clean.tsv")
-time_gender_df    <- read_tsv("data/time_gender_data_clean.tsv")
-time_province_df  <- read_tsv("data/time_province_data_clean.tsv")
-search_trend_df   <- read_tsv("data/search_trend_data_clean.tsv")
+# Augment time dataframes -----------------------------------------------------------------------
 
 # Full join time series data by date. Suffix are added for col collisions
 time_df <- time_df %>%
-  full_join(time_age_df,
-    by = "date",
-    suffix = c("", "_time_age")
-  ) %>%
-  full_join(time_gender_df,
-    by = "date",
-    suffix = c("", "_time_gender")
-  ) %>%
-  full_join(time_province_df,
-    by = "date",
-    suffix = c("", "_time_province")
-  ) %>%
+  full_join(time_age_df, by = "date", suffix = c("", "_time_age")) %>%
+  full_join(time_gender_df, by = "date", suffix = c("", "_time_gender")) %>%
+  full_join(time_province_df, by = "date", suffix = c("", "_time_province")) %>%
   full_join(search_trend_df, by = "date")
 
 # Removing unwanted columns
@@ -112,8 +107,7 @@ time_df <- time_df %>%
     pneumonia
   ))
 
-# Region augment
-city_df <- read_tsv("data/city_data_clean.tsv")
+# Augment region dataframe ----------------------------------------------------------------------
 
 # Removing unwanted columns
 city_df <- city_df %>% 
@@ -132,16 +126,9 @@ city_df <- confirmed_cases %>%
   mutate(confirmed = replace_na(confirmed, 0)) %>%
   drop_na()
 
-# Write data to file --------------------------------------------
+# Write data to file ----------------------------------------------------------------------------
 
-# Write to disk
 write_tsv(case_df, "data/case_data_augmented.tsv")
-
-# Write to disk
 write_tsv(patient_df, "data/patient_data_augmented.tsv")
-
-# Write to disk
 write_tsv(time_df, "data/time_data_augmented.tsv")
-
-# Write to disk
 write_tsv(city_df, "data/city_data_augmented.tsv")
